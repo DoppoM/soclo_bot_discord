895cead087813f70f37ef41e352c9b10e4982328
mbash <gicata.sr@gmail.com>
2020-05-31 04:40:17 +0300

    add command for plaing soundcloud tracks add ffmpeg for Win

--- bot/bot.py
+++ bot/bot.py
@@ -3,10 +3,12 @@ import random
 import time
 import json
 import asyncio
-import livechecker
+import glob
+import livechecker                  #my module
+import scexec                       #my module
 from discord.ext import commands
 
-discordToken = ''
+discordToken = 'NjkwOTIyNDQ2MDM1MDkxNTM2.XtL5hg.4BMMBzNIPfWkaUu9G_kJ5SJ0kok'
 notificationChID = '281868313674645504'
 
 discordTtwTrigger = ''
@@ -50,12 +52,28 @@ async def dice(ctx, arg):
         result = dice1 + dice2
         await ctx.send(':game_die: is {}, :game_die: is {}, :game_die::game_die: is {}'.format(dice1, dice2, result))
 
-@bot.command(name = 'twtest')
-async def twtest(ctx):
-    print(twitchOnlineCheck.twitchIsStarted)
-    if twitchOnlineCheck.twitchIsStarted == 'y':
-        await ctx.send("+")
-    if twitchOnlineCheck.twitchIsStarted == 'n':
-        await ctx.send("-")
+@bot.command(pass_context=True)
+async def scloud(ctx, arg_uri):
+    music = scexec.scdownloader(arg_uri)
+    author = ctx.author
+    channel = author.voice.channel
+    vc = await channel.connect()
+    vc.play(discord.FFmpegPCMAudio(executable="./ffmpeg-static/bin/ffmpeg.exe", source=music.mp3_count[0]), after=lambda e: print('done', e))   
+
+@bot.command(pass_context=True)
+async def scstop(ctx):
+    #author = ctx.author
+    #channel = author.voice.channel
+    bot.voice_clients.remove
+    for f in glob.glob('./bot/music/*.mp3'):
+           os.unlink(f)
+
+#@bot.command(name = 'twtest')
+#async def twtest(ctx):
+#    print(twitchOnlineCheck.twitchIsStarted)
+#    if twitchOnlineCheck.twitchIsStarted == 'y':
+#        await ctx.send("+")
+#    if twitchOnlineCheck.twitchIsStarted == 'n':
+#       await ctx.send("-")
 
 bot.run(discordToken)
--- bot/scexec.py
+++ bot/scexec.py
@@ -4,14 +4,18 @@ import asyncio
 import webbrowser
 import string
 import sys
+import glob
+import os
 from soundscrape.soundscrape import process_soundcloud
 
-uri = 'https://soundcloud.com/blessxshahmen/shahmen-1981985-pt-2-black-flowers'
+# uri = 'https://soundcloud.com/kkuko/nkoha-black-swordsman'  # uri for test
 class scdownloader(object):
     def __init__(self, m_uri):
-        #browser = webbrowser.get('windows-default')
-        #browser.open(m_uri + '/play')
         print (m_uri)
-        vargs = {'path':'', 'folders': False, 'group': False, 'track': '', 'num_tracks': 9223372036854775807, 'bandcamp': False, 'downloadable': False, 'likes': False, 'open': False, 'artist_url': m_uri, 'keep': True}
-        sound = process_soundcloud (vargs)
-
+        for f in glob.glob('./bot/music/*.mp3'):
+           os.unlink(f)
+        mp3_count = glob.glob1('', "./bot/music/*.mp3")  
+        vargs = {'path':'./bot/music/', 'folders': False, 'group': False, 'track': '', 'num_tracks': 9223372036854775807, 'bandcamp': False, 'downloadable': False, 'likes': False, 'open': False, 'artist_url': m_uri, 'keep': True}
+        process_soundcloud(vargs)
+        self.mp3_count = glob.glob("./bot/music/*.mp3")
+#test = scdownloader(uri)
